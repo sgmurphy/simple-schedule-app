06b21ebe51530ed234462a0a823c55a1cfc0f28a
Sean Murphy <sean@iamseanmurphy.com>
2016-07-22 18:13:15 -0400

    Remove records from the in-memory collection when deleting them.

--- app/controllers/groups/index.js
+++ app/controllers/groups/index.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     delete(group) {
+      this.get('model').removeObject(group);
       group.destroyRecord();
     }
   }
