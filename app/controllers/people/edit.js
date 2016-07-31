afc41d233bd16459df72ca5e6fc3a7067ec5d505
Sean Murphy <sean@iamseanmurphy.com>
2016-07-31 16:37:27 -0400

    Use emberx-select for select fields

--- app/controllers/people/edit.js
+++ app/controllers/people/edit.js
@@ -3,19 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   assignmentFrequencies: [0, 1, 2, 3, 4, 5, 6, 7, 8],
   actions: {
-    changeGroups(element) {
-      var controller = this;
-      var groups = [];
-      Ember.$(":selected", element).each(function() {
-        groups.push(controller.store.peekRecord('group', Ember.$(this).val()));
-      });
-
-      this.model.set('groups', groups);
-    },
-    changeAssignmentFrequency(freq) {
-      this.model.set('assignmentFrequency', freq);
-    },
     save() {
+      this.groups.invoke('save');
       this.model.save();
       this.transitionToRoute('people');
     },
